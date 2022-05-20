5dbb6102c2c0429fa699d58c60a66e104f25dd40
Ashutosh Singh Parmar <51224274+parmAshu@users.noreply.github.com>
2022-05-20 18:54:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# py_simple_serial 
+
+This is a python package to communicate with a device using simple serial over uart.
+
+![simple_serial_state_machine](docs/simple_serial_state_machine.png)
---
+++ simpleSerialConstants.py
@@ -0,0 +1,13 @@
+"""
+@author : Ashutosh.p
+@date : 26-04-2022
+@brief : This file contains all the constants required for simple serial module
+"""
+# Constant to define simple serial preamble byte
+SIMPLE_SERIAL_PREAMBLE_BYTE = b'\xAA'
+
+# Constant to define simple serial version 1 byte
+SIMPLE_SERIAL_VERSION_1 = b'\x01'
+
+# Constant to defnie simple serial frame end byte
+SIMPLE_SERIAL_FRAME_END = b'\x1e'
